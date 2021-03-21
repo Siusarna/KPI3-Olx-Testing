db85e42db6121d40686b245bfbbc13762db077a7
Siusarna <42269105+Siusarna@users.noreply.github.com>
2021-03-22 01:30:46 +0200

    fix naming

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -4,21 +4,15 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.HomePage;
-import pages.LaunchWebPlayerPage;
-import pages.LoginPage;
 
 public class SearchTest {
     private WebDriver driver;
     private WebDriverWait wait;
-    private LaunchWebPlayerPage page;
     private HomePage homePage;
 
     @Before
